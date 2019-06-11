209c1acaaa3885693203a6c0f809d42150c3e6d7
Victor Johnson <victorjohnson@victors-mbp.lan>
2019-06-11 12:45:19 -0400

    created game logic

--- Memory_Game/JS/main.js
+++ Memory_Game/JS/main.js
@@ -2,18 +2,22 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You've found a match");
+	} else {
+		console.log("Sorry, try again");
+	}
+};
 
-console.log("User flipped" + " " + cardOne);
+function flipCard(cardID){
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+};
+
+	
+flipCard(0);
+flipCard(1);
 
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped" + " " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match");
-} else {  alert("Sorry, try again");
-}
